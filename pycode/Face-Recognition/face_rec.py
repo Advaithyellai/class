ccd2d730be95ddd309b737dbd5a3bf8c4ac4fea7
Advaith Yellai <adithi.advaith@gmail.com>
2021-06-08 12:45:27 +0530

    made face_rec and written Recognition

--- pycode/Face-Recognition/face_rec.py
+++ pycode/Face-Recognition/face_rec.py
@@ -1,79 +1,3 @@
-# import cv2
-
-# face_cascade = cv2.CascadeClassifier('face-data//Image_face.xml')
-# eye_cascade = cv2.CascadeClassifier('face-data//all_eye.xml')
-
-# img = cv2.imread('face-data//face_1.jpg')
-
-# faces = face_cascade.detectMultiScale(img, 1.1, 4)
-# eyes = eye_cascade.detectMultiScale(img, 1.1, 4)
-
-# for (x, y, w, h) in faces: cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-# for (x, y, w, h) in eyes: cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-
-# cv2.imshow("Faces and eyes found", img)
-
-# print('Found {} faces'.format(len(faces)))
-
-# ok_flag = True
-# while ok_flag:
-#     if cv2.waitKey(0) == ord('q'): ok_flag = False
-
-# cv2.destroyAllWindows()
-
-
-# import numpy as np
-# import face_recognition
-# import cv2
-
-# path = input("Type in the selected image:   ")
-
-# img = cv2.imread("dataImages//{}.jpg".format(path))
-# bw_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-# faceLoc = face_recognition.face_locations(bw_img)[0]
-# faceenc = face_recognition.face_encodings(bw_img)
-# faceEnc = list(faceenc[0])
-
-# f = open("faceData.txt", "r")
-# fr = f.read()
-
-# for line in fr.split("\n"):
-#     try:
-#         if line == "":
-#             sameface = False
-#             break
-#         line2 = eval(line)
-#         line = line2[0:-1]
-#         sameface = face_recognition.compare_faces([np.array(line)], faceenc[0])[0]
-#         if sameface: break
-#     except Exception as ex: pass
-
-# f.close() 
-
-# if sameface:
-#     f2 = open("faceData.txt", "r")
-#     cv2.putText(img, line2[-1], (faceLoc[3]-5, faceLoc[0]-20),\
-#         cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0))
-
-# else: 
-#     f2 = open("faceData.txt", "a")
-#     name = input("Who is in the image?   ")
-#     f2.write(str(faceEnc+[name])+"\n")
-#     print("saved the image successfuly")
-#     cv2.putText(img, name, (faceLoc[3]-5, faceLoc[0]-20),\
-#         cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0))
-
-# f2.close()
-
-# cv2.rectangle(img, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (0, 0, 0), 2)
-
-# cv2.imshow("Face", img)
-# cv2.waitKey(0)
-
-
-
 import face_recognition
 import cv2
 import numpy as np
@@ -103,9 +27,9 @@ counter = correct = incorrect = c = 0
 text = "  "
 penc = -1
 
-f = open("faceData.txt", "r")
+f = open("D:\\Advaith\\Code\\class\\pycode\\Recognition\\faceData.txt", "r")
 fr = f.read()
-f2 = open("faceData.txt", "r")
+f2 = open("D:\\Advaith\\Code\\class\\pycode\\Recognition\\faceData.txt", "r")
 
 lenc = []
 
